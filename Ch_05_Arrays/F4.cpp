a4097f1602c4847267d1bb20e30cfecf0019cdde
Ahmed Raza Qureshi <109075750+AhmedRazaQureshi@users.noreply.github.com>
2024-07-24 19:56:51 +0000

    Ch_05

--- Ch_05_Arrays/F4.cpp
+++ Ch_05_Arrays/F4.cpp
@@ -1,6 +1,7 @@
 //Questions on Array/Vectors
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 //Ques#01: Sort array of 0's, 1's & 2's [Sort Colors] (inplace)
@@ -50,9 +51,65 @@ void sortColorsMethod2(vector<int>& nums)
 
     while(mid <= high)
     {
-        
+        if(nums[mid] == 0)
+        {
+            swap(nums[mid], nums[low]);
+            mid++;
+            low++;
+        }
+        else if(nums[mid] == 1)
+        {
+            mid++;
+        }
+        else if(nums[mid] == 2)
+        {
+            swap(nums[mid], nums[high]);
+            high--;
+            //Not doing mid++ bec. agar swap k baad mid pe 2 aa gya to?
+        }
     }
 }
+
+
+//Ques#02: Add two numbers represented by two arrays (each digit is in a cell of array)
+vector<int> addTwoNumbersByArray(const vector<int>& nums1, const vector<int>& nums2)
+{
+    int i = nums1.size() - 1; // Pointer to the rightmost cell of array1
+    int j = nums2.size() - 1; // Pointer to the rightmost cell of array2
+    int carry = 0;
+    vector<int> result;
+
+    // The loop for addition
+    while (i >= 0 || j >= 0 || carry)
+    {
+        int sum = carry;
+        if (i >= 0) sum += nums1[i--];
+        if (j >= 0) sum += nums2[j--];
+
+        result.push_back(sum % 10);
+        carry = sum / 10;
+    }
+
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+
+//Ques#03: Factorial of large no.s (using vector)
+vector<int> findFactorialByVector(int n)
+{
+    vector<int> fact;
+    
+    
+
+
+
+
+
+
+    return fact;
+}
+
 
 int main()
 {
@@ -74,6 +131,26 @@ int main()
     for(auto i: nums1)
         cout<<i<<", ";
     cout<<endl<<endl;
+
+    
+    //Ques#02: Add two numbers represented by two arrays (each digit is in a cell of array)
+    vector<int> nums2 = {9, 5, 4, 9};
+    vector<int> nums3 = {2, 1, 4};
+    vector<int> sum = addTwoNumbersByArray(nums2, nums3);
+    cout<<"Sum of nums2 & nums3: ";
+    for(auto i: sum)
+        cout<<i;
+    cout<<endl<<endl;
+
+
+    //Ques#03: Factorial of large no.s (using vector)
+    int number = 7;
+    vector<int> factorial = findFactorialByVector(7);
+    cout<<"Factorial of 7: ";
+    for(auto i: factorial)
+        cout<<i;
+    cout<<endl<<endl;
+
 
     return 0;
 }
